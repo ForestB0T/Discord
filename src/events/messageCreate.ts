5845a093433c23610e3b05ff7f5e9d9cf2d6e80d
Febzey <braydent1235@gmail.com>
2023-01-15 23:57:42 -0600

    added a status checker

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,8 +1,27 @@
 import type { Message } from "discord.js"
+import fetchData from "../fuctions/fetch.js";
 import type ForestBot from "../structure/discord/Client"
 import makeTablistEmbed from "../utils/embeds/make_tablist_embed.js";
 
 const prefix = "!";
+
+
+async function statusEmbedBuilder() {
+    const data = await fetchData("https://api.forestbot.org/status") as any;
+    const value = data.status.connectedServers.map(server => `- ${server}`).join('\n');
+    const embed = {
+        title: "API Status",
+        fields: [
+            { name: 'Connected Servers', value: value.length > 0 ? `\`\`\`\n${value}\n\`\`\`` : 'No servers connected' },
+            { name: 'Database Connection', value: data.status.databaseIsConnected ? 'Connected' : 'Disconnected' },
+            { name: 'Memory Usage', value: data.status.memory },
+            { name: 'Messages Saved', value: (data.status.messages.toLocaleString()).toString() , inline: true },
+            { name: 'Users Saved', value: (data.status.users.toLocaleString()).toString(), inline: true }
+        ]
+    };
+
+   return embed;
+}
 
 export default {
     name: "messageCreate",
@@ -19,6 +38,14 @@ export default {
             return channel.send(makeTablistEmbed(server, server+"_refresh"));
         }
 
+
+        if (content.startsWith(prefix + "status")) {
+            const msg = await channel.send({ embeds: [await statusEmbedBuilder()] });
+            setInterval(async () => {
+                await msg.edit({ embeds: [await statusEmbedBuilder()] });
+            }, 200000);
+        }
+    
 
     }
 }
--- src/fuctions/fetch.ts
+++ src/fuctions/fetch.ts
@@ -1,7 +1,4 @@
 //import fetch
 import fetch from 'node-fetch';
-
-
-const fetchData = async (url: string) => await (await fetch(url)).json()
-
+const fetchData = async (url: string) => await (await fetch(url)).json();
 export default fetchData
