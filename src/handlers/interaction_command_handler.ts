fb9ebb06825bf0bcbe172c199e11aee409c5a2aa
Febzey <braydent1235@gmail.com>
2023-04-03 15:08:24 -0500

    ww

--- src/handlers/interaction_command_handler.ts
+++ src/handlers/interaction_command_handler.ts
@@ -31,7 +31,7 @@ export default async function commandHandler(interaction: CommandInteraction, cl
         if (typeof permissions === "string") permissions = [permissions];
         for (const perm of permissions) {
 
-            if (member.roles.cache.some(role => role.permissions.has(Permissions.FLAGS[perm]))) return run(interaction, client, thisGuild);
+            if (member.roles.cache.some(role => role.permissions.has(perm))) return run(interaction, client, thisGuild);
 
             if (!member.permissions.has(perm)) {
                 client.ErrorHandler.noUserPermission(interaction, perm);
