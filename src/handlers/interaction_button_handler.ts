0d2a6f3582e3cc09bc368ae5f81c01a57fa37e67
febzey <braydent1235@gmail.com>
2024-12-22 13:44:54 -0600

    normalize casing for server name in button handler refresh interaction

--- src/handlers/interaction_button_handler.ts
+++ src/handlers/interaction_button_handler.ts
@@ -14,6 +14,6 @@ export default async function buttonHandler(interaction: Interaction, client: Fo
     }
 
     if (interaction["customId"] === "refresh" || interaction["customId"] === thisGuild.mc_server) {
-        return await interaction.update(await makeTablistEmbed(thisGuild.mc_server, "refresh"));
+        return await interaction.update(await makeTablistEmbed(thisGuild.mc_server.toLowerCase(), "refresh"));
     }
 };
